d7bfb487ff068d757c8322ad2af467bea665f6e5
nanato12 <admin@nanato12.info>
2021-11-04 04:29:39 +0900

    update: main

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-# main python file
+from othello import Borad
+from othello.errors import (
+    AlreadyPutError,
+    ImpossibleReverseError,
+    InvalidRangeError,
+    NoEffectPointError,
+)
+from othello.models import Stone
+
+board = Borad()
+for color in board.order():
+    board.show()
+    while True:
+        try:
+            y, x = input(
+                f"[{color.icon}] {color.name} input (x,y) >>> "
+            ).split(",")
+            board.put_stone(Stone(int(x) - 1, int(y) - 1, color))
+            break
+        except ValueError:
+            print("カンマ区切りの数字で入力してくれないかね？ 入力例はこんな感じ -> 1,1")
+        except (
+            InvalidRangeError,
+            AlreadyPutError,
+            NoEffectPointError,
+            ImpossibleReverseError,
+        ) as e:
+            print(e)
+        except Exception:
+            raise
